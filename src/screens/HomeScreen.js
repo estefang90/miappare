64768d08a80e3cfd17ae873575d4922cd4a1206c
estefang90 <36801483+estefang90@users.noreply.github.com>
2024-02-08 11:27:42 -0500

    app con react

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen(props) {
+
+    const { navigation } = props;
+
+    const goToSettings = () => {
+        navigation.navigate("Settings")
+    }
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Button onPress={goToSettings} title='Ir a Ajuste' />
+    </View>
+  )
+}
